282231f1d52f14b0b8a2456cdef82bbf0fc4c7ca
Rupkuvarba <baradrupce@gmail.com>
2020-12-25 00:07:03 +0530

    Scaffold-appbar-body

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,23 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(
-      Center(
-        child: Text(
-            "Hello Flutter",
-            textDirection: TextDirection.ltr
-        ),
-      )
+
+     MaterialApp(
+       title: "My Flutter App",
+       home: Scaffold(
+         appBar: AppBar(title: Text("My First App Screen"),),
+         body: Material(
+           color: Colors.orangeAccent,
+           child: Center(
+             child: Text(
+               "Hello Flutter",
+               textDirection: TextDirection.ltr,
+               style: TextStyle(color: Colors.white, fontSize: 40.0),
+             ),
+           ),
+         ),
+       )
+     )
+
   );
 }
