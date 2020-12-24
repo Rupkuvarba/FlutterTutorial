8bf83440119fda911c4db0fd7fe5200573e27732
Rupkuvarba <baradrupce@gmail.com>
2020-12-25 00:24:05 +0530

    main function seprated in class

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(
+void main() => runApp(new MyFlutterApp());
 
-     MaterialApp(
-       title: "My Flutter App",
-       home: Scaffold(
-         appBar: AppBar(title: Text("My First App Screen"),),
-         body: Material(
-           color: Colors.orangeAccent,
-           child: Center(
-             child: Text(
-               "Hello Flutter",
-               textDirection: TextDirection.ltr,
-               style: TextStyle(color: Colors.white, fontSize: 40.0),
-             ),
-           ),
-         ),
-       )
-     )
+class MyFlutterApp extends StatelessWidget{
 
-  );
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,  //Remove debug tag
+        title: "My Flutter App",
+        home: Scaffold(
+          appBar: AppBar(title: Text("My First App Screen"),),
+          body: Material(
+            color: Colors.orangeAccent,
+            child: Center(
+              child: Text(
+                "Hello Flutter",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(color: Colors.white, fontSize: 40.0),
+              ),
+            ),
+          ),
+        )
+    );
+
+  }
+
 }
