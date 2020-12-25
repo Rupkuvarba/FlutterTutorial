c744a2c95896bc41c9ba7ca532d6f4559c6f07e5
Rupkuvarba <baradrupce@gmail.com>
2020-12-25 22:24:30 +0530

    2.3 Flutter Custom Fonts in Text widget. Text Style in Flutter

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -9,9 +9,18 @@ class Home extends StatelessWidget{
       child: Container(
         alignment: Alignment.center,
         color: Colors.deepPurple,
-        margin: EdgeInsets.only(left:35.0, top:50.0),
-        //padding: EdgeInsets.all(10.0),
-        child: Text("Flight", textDirection: TextDirection.ltr),
+        child: Text(
+          "Flight",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+              decoration: TextDecoration.none,
+              fontSize: 75.0,
+              fontFamily: "DMSans",
+              fontWeight: FontWeight.w500,
+              fontStyle: FontStyle.italic,
+              color: Colors.white
+          ) ,
+        ),
       ),
     );
 
