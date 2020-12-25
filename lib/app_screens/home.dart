8bb2dc95a5691b27ba9f6af8263c52371f8c2fdf
Rupkuvarba <baradrupce@gmail.com>
2020-12-25 23:08:10 +0530

    2.4-2: Row, Columns and expandable widget

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -1,51 +1,81 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget{
-
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Center(
       child: Container(
+        padding:
+            EdgeInsets.only(left: 10.0, top: 50.0, right: 10.0, bottom: 50.0),
         alignment: Alignment.center,
         color: Colors.deepPurple,
-        child: Row(
-           children: [
 
-             Expanded(child: Text(
-               "Flight",
-               textDirection: TextDirection.ltr,
-               style: TextStyle(
-                   decoration: TextDecoration.none,
-                   fontSize: 35.0,
-                   fontFamily: "DMSans",
-                   fontWeight: FontWeight.w500,
-                   fontStyle: FontStyle.italic,
-                   color: Colors.white
-               ) ,
-             )),
+        //column
+        child: Column(
+          children: [
+            //Column-1
+            Row(
+              children: [
+                Expanded(
+                    child: Text(
+                  "Flight",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 35.0,
+                      fontFamily: "DMSans",
+                      fontWeight: FontWeight.w500,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.white),
+                )),
+                Expanded(
+                    child: Text(
+                  "Flight1 which is coming from banglore",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 20.0,
+                      fontFamily: "DMSans",
+                      fontWeight: FontWeight.w500,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.white),
+                ))
+              ],
+            ),
 
-             Expanded(child:
-             Text(
-               "Flight1 which is coming from banglore",
-               textDirection: TextDirection.ltr,
-               style: TextStyle(
-                   decoration: TextDecoration.none,
-                   fontSize: 20.0,
-                   fontFamily: "DMSans",
-                   fontWeight: FontWeight.w500,
-                   fontStyle: FontStyle.italic,
-                   color: Colors.white
-               ) ,
-             )
-             )
-
-           ],
-        )
-
+            //column 2
+            Row(
+              children: [
+                Expanded(
+                    child: Text(
+                  "Vegetable",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 35.0,
+                      fontFamily: "DMSans",
+                      fontWeight: FontWeight.w500,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.white),
+                )),
+                Expanded(
+                    child: Text(
+                  "Vegetables grows in land. It will help us alot",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 20.0,
+                      fontFamily: "DMSans",
+                      fontWeight: FontWeight.w500,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.white),
+                ))
+              ],
+            )
+          ],
+        ),
       ),
     );
-
   }
-
 }
