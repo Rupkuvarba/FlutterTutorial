7aad1c8b4cb812b32e7c621cbc434ee34a7d346a
Rupkuvarba <baradrupce@gmail.com>
2020-12-25 21:43:52 +0530

    Container: Set width and height

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -9,6 +9,8 @@ class Home extends StatelessWidget{
       child: Container(
         alignment: Alignment.center,
         color: Colors.deepPurple,
+        width: 200.0,
+        height: 100.0,
         child: Text("Flight", textDirection: TextDirection.ltr),
       ),
     );
