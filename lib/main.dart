bda13d690f5a84bec4b1a995bda37ec0334bcfea
Rupkuvarba <baradrupce@gmail.com>
2020-12-27 17:54:51 +0530

    2.7: Basic Listview

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,54 @@ import 'package:flutter_app_tutorial/app_screens/home.dart';
 void main(){
   runApp(MaterialApp(
     title: "Exploring UI widgets",
-    home: Home(),
+    home: Scaffold(
+      appBar: AppBar(title: Text("Basic ListView"),),
+      body: getListview(),
+    ),
   ));
+}
+
+Widget getListview(){
+
+  var listView = ListView(
+    children: <Widget>[
+
+      ListTile(
+        leading: Icon(Icons.landscape),
+        title: Text("Landscape"),
+        subtitle: Text("Beautiful view"),
+        trailing: Icon(Icons.wb_sunny),
+        onTap: (){
+          debugPrint("Landscape tapped");
+        },
+      ),
+
+      ListTile(
+        leading: Icon(Icons.laptop_chromebook),
+        title: Text("Mac"),
+        trailing: Icon(Icons.wb_cloudy),
+      ),
+
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Phone"),
+      ),
+
+      ListTile(
+        title: Text("Only title"),
+      ),
+
+      Container(
+        child: Text("Yet another element in list",),
+        margin: EdgeInsets.all(10.0),
+      ),
+
+      Container(margin: EdgeInsets.all(10.0), color: Colors.red, height: 30,),
+
+    ],
+  );
+
+  return listView;
 }
 /*import 'package:flutter_app_tutorial/app_screens/first_screen.dart';
 
