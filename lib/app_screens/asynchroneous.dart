ac804d4634e866a760874f119507ac18d92bf110
Rupkuvarba <baradrupce@gmail.com>
2020-12-31 20:21:50 +0530

    4.6: SQFLite-Create model class to map sqlite db

---
+++ lib/app_screens/asynchroneous.dart
@@ -0,0 +1,41 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app_tutorial/app_screens/note_list.dart';
+
+void main(){
+  //runApp(MyApp());
+  print('main programm starts');
+  printFileContent();
+  print('main programm ends');
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "NoteKeeper",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: NoteList(),
+    );
+  }
+}
+
+printFileContent(){
+  Future<String> fileContent = downLoadAFile();
+  fileContent.then((resultValue) => print('The content of the file is resultValue --> $resultValue'));
+  //print('the content of the file is --> $fileContent');
+}
+
+//To download a file [Dummy long running operation]
+Future<String> downLoadAFile(){
+
+  Future<String> result = Future.delayed(Duration(seconds: 6), () {
+    return 'My secret file content';
+  });
+
+  return result;
+}
---
+++ lib/models/note.dart
@@ -0,0 +1,68 @@
+
+class Note{
+
+  int _id;
+  String _title;
+  String _description;
+  String _date;
+  int _priority;
+
+  Note(this._title, this._date, this._priority, [this._description]);  // description optional
+
+  Note.withId(this._id, this._title, this._date, this._priority, [this._description]);  // description optional
+
+  int get priority => _priority;
+
+  String get date => _date;
+
+  String get description => _description;
+
+  String get title => _title;
+
+  int get id => _id;
+
+  set priority(int newPriority) {
+    _priority = newPriority;
+  }
+
+  set date(String newDate) {
+    _date = newDate;
+  }
+
+  set description(String newDescription) {
+    if(newDescription.length <= 255) {
+      _description = newDescription;
+    }
+  }
+
+  set title(String newTitle) {
+    if(newTitle.length <= 255) {
+      _title = newTitle;
+    }
+  }
+
+  //Convert a Note object into a Map object
+  Map<String, dynamic> toMap(){
+    var map = Map<String, dynamic>();
+    if(id != null){
+      map['id'] = _id;
+    }
+
+    map['title'] = _id;
+    map['description'] = _id;
+    map['priority'] = _id;
+    map['date'] = _id;
+
+    return map;
+  }
+
+  //Extract a Note object from a Map object
+  Note.fromMapObject(Map<String, dynamic> map){
+    this._id = map['id'];
+    this._title = map['title'];
+    this._description = map['description'];
+    this._priority = map['priority'];
+    this._date = map['date'];
+  }
+
+}
