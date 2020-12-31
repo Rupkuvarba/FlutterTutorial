712326378ea68a0e8cbb0c55f4027fba28df3470
Rupkuvarba <baradrupce@gmail.com>
2020-12-31 12:13:21 +0530

    4.4: Asynchronous: Future, Async and await

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app_tutorial/app_screens/note_list.dart';
 
 void main(){
-  runApp(MyApp());
+  //runApp(MyApp());
+  print('main programm starts');
+  printFileContent();
+  print('main programm ends');
 }
 
 class MyApp extends StatelessWidget{
@@ -19,5 +22,20 @@ class MyApp extends StatelessWidget{
       home: NoteList(),
     );
   }
+}
 
+printFileContent() async{
+  //Future<String> fileContent = downLoadAFile();
+  String fileContent = await downLoadAFile();
+  print('the content of the file is --> $fileContent');
+}
+
+//To download a file [Dummy long running operation]
+Future<String> downLoadAFile(){
+
+  Future<String> result = Future.delayed(Duration(seconds: 6), () {
+    return 'My secret file content';
+  });
+
+  return result;
 }
